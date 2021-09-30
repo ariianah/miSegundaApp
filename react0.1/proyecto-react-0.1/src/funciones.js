e668a9d034ed362ce6a59beeaa23e54bdb981b32
Ariana <arianagonzalez.2094@gmail.com>
2021-09-30 18:20:58 +0200

    mi commit ariana

---
+++ react0.1/proyecto-react-0.1/src/funciones.js
@@ -0,0 +1,6 @@
+function Funciones() {
+    return (
+        <p>Hola mundo</p>
+    )
+}
+export default Funciones;
---
+++ react0.1/proyecto-react-0.1/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import Funciones from './funciones';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Funciones />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
